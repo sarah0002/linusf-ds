3b3ecd964962056ef74f0d6e48d0d771469d5fc0
Sarah Albaek <saal0002@stud.kea.dk>
2025-06-28 21:55:57 +0200

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; 
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: { 
+            colors: {
+            'baggrund': '#32557B',
+            'farve1': '#252422',
+            'farve2': '#D66445',
+            'farve3': '#E7E6E3',
 
-import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
-export default defineConfig({
-  vite: {  
-    plugins: [tailwindcss()],
-  },
-  integrations: [alpinejs()],
-});
+          },},
+	},
+	plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/container-queries'),
+    ],
+}
